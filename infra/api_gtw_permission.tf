a169812f5a646475af54ba06cdbc11467344720c
Fabio Marques <46504380+Marques0x01@users.noreply.github.com>
2024-05-01 14:48:11 -0300

    Feature/changing accoung (#11)
    * changing tfstate bucket
    
    * changing lambda
    
    * updating s3 backend
    
    * fixing lambda arn
    
    * feat: add orders endpoint
    
    * feat: changing parent id auth
    
    * changing backend
    
    * change lambdas arn
    
    * add logs
    
    * fix stage
    
    * add role cw

--- infra/api_gtw_permission.tf
+++ infra/api_gtw_permission.tf
@@ -24,3 +24,51 @@ resource "aws_lambda_permission" "apigw_order" {
 }
 
 
+resource "aws_iam_role" "cloudwatch_logs" {
+  name               = "apigateway-cloudwatch-logs-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_apigateway_account" "example" {
+  cloudwatch_role_arn = aws_iam_role.cloudwatch_logs.arn
+}
+
+
+
+resource "aws_iam_policy" "cloudwatch_logs_policy" {
+  name        = "apigateway-cloudwatch-logs-policy"
+  description = "Policy for API Gateway to write logs to CloudWatch Logs"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs_policy_attachment" {
+  role       = aws_iam_role.cloudwatch_logs.name
+  policy_arn = aws_iam_policy.cloudwatch_logs_policy.arn
+}
