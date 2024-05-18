e9930b29e8e7268d6db711fb397d1dfa438df7fb
leonardo rodrigues <leonardorodrigues_f@hotmail.com>
2024-05-18 17:34:26 -0300

    ajuste na rota de pagamentos

--- infra/api_gtw_endpoint_lambda_payment_create.tf
+++ infra/api_gtw_endpoint_lambda_payment_create.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_method" "payment_create_post" {
   rest_api_id   = aws_api_gateway_rest_api.api_gtw.id
-  resource_id   = aws_api_gateway_resource.proxy_order.id
+  resource_id   = aws_api_gateway_resource.proxy_payment.id
   http_method   = "POST"
   authorization = "NONE"
 
