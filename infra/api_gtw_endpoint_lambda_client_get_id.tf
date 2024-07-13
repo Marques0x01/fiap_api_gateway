0ee6c870f2d493b68d3e80b416ca280e86fa5022
Fabio Marques <46504380+Marques0x01@users.noreply.github.com>
2024-07-13 19:49:21 -0300

    Feature/new account (#30)
    * Change lambdas
    
    * commenting logs
    
    * change backend
    
    * logs
    
    * change account
    
    * change backend
    
    * change region
    
    * removing logs
    
    * change account
    
    * .
    
    * account
    
    * .
    
    * .

--- infra/api_gtw_endpoint_lambda_client_get_id.tf
+++ infra/api_gtw_endpoint_lambda_client_get_id.tf
@@ -14,7 +14,7 @@ resource "aws_api_gateway_resource" "client_by_id" {
 }
 
 
-resource "aws_api_gateway_integration" "lambda_order_unfinished" {
+resource "aws_api_gateway_integration" "client_by_id" {
   rest_api_id = aws_api_gateway_rest_api.api_gtw.id
   resource_id = aws_api_gateway_method.client_by_id.resource_id
   http_method = aws_api_gateway_method.client_by_id.http_method
