2c7dfbd74129f0afb06939c9852de4ee8a21a0c8
Fabio Marques <fabio.marquezzx@gmail.com>
2024-07-14 15:12:36 -0300

    update resource path part

--- infra/api_gtw_endpoint_lambda_kitchen_update_status.tf
+++ infra/api_gtw_endpoint_lambda_kitchen_update_status.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_method" "kitchen_update" {
   rest_api_id   = aws_api_gateway_rest_api.api_gtw.id
-  resource_id   = aws_api_gateway_resource.proxy_kitchen.id
+  resource_id   = aws_api_gateway_resource.put_order_status_kitchen.id
   http_method   = "PUT"
   authorization = "NONE"
 
@@ -13,6 +13,14 @@ resource "aws_api_gateway_method" "kitchen_update" {
   }
 }
 
+
+resource "aws_api_gateway_resource" "put_order_status_kitchen" {
+  rest_api_id = aws_api_gateway_rest_api.api_gtw.id
+  parent_id   = aws_api_gateway_resource.proxy_kitchen.id
+  path_part   = "order-status"
+
+  depends_on = [aws_api_gateway_rest_api.api_gtw, aws_api_gateway_resource.proxy_kitchen]
+}
 
 resource "aws_api_gateway_integration" "lambda_kitchen_put" {
   rest_api_id = aws_api_gateway_rest_api.api_gtw.id
